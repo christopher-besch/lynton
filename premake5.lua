61179c9295abacbc9b910e5658ebc26eda511051
Christopher Besch <christopher.besch@gmx.de>
2021-05-07 13:16:16 +0200

    c++ api

--- premake5.lua
+++ premake5.lua
@@ -6,6 +6,12 @@ workspace "lynton"
     configurations { "Debug", "Release", "Dist" }
     platforms { "WASM", "Linux", "Windows" }
 
+    -- wasm doesn't support 64bit
+    filter { "platforms:WASM" }
+        architecture "x86"
+    filter { "not platforms:WASM" }
+        architecture "x86_64"
+
     filter "configurations:Debug"
         defines { "DEBUG" }
         symbols "On"
@@ -27,10 +33,6 @@ workspace "lynton"
 
 project "lynton"
     language "C++"
-    -- filter { "platforms:WASM" }
-    --     architecture "x86"
-    filter {}
-        architecture "x86_64"
     cppdialect "c++latest"
     kind "ConsoleApp"
 
@@ -41,14 +43,14 @@ project "lynton"
     files {
         "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"
     }
-    
+
     includedirs {
         "%{prj.name}/src",
     }
 
     filter "platforms:WASM"
+        linkoptions { "-s WASM=1", "-s EXPORTED_FUNCTIONS='[\"_main\", \"_fib\"]'", "-s EXPORTED_RUNTIME_METHODS='[\"cwrap\"]'" }
         targetname ("%{prj.name}.html")
-        linkoptions { "-mwasm64" }
     filter "platforms:Linux"
         targetname ("%{prj.name}")
     filter "platforms:Windows"
