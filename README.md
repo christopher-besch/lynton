05afa790a1d1eb4288aa422ea664ef302cda964b
Christopher Besch <christopher.besch@gmx.de>
2021-06-06 08:51:54 +0200

    build for linux works

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Lynton Game Engine
 
 This repository is under construction and not in a usable state.
+
+## Dependencies
+
+- SDL2, can be installed with `sudo apt install libsdl2-dev`
--- lynton/premake5.lua
+++ lynton/premake5.lua
@@ -11,11 +11,16 @@ project "lynton"
         "src/**.h", "src/**.cpp"
     }
 
+    includedirs {
+        "%{wks.location}/utils/src"
+    }
+
     filter "platforms:WASM"
         linkoptions { "-s WASM=1", "-s EXPORTED_FUNCTIONS='[\"_main\"]'", "-s EXPORTED_RUNTIME_METHODS='[\"cwrap\"]'", "-s USE_SDL=2" }
         targetname ("%{prj.name}.html")
     filter "platforms:Linux"
         links { "SDL2" }
+        links { "SDL2main" }
         targetname ("%{prj.name}")
     -- not supported
     filter "platforms:Windows"
